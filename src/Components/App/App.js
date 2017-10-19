e0d7973ef8ea5858e0f0a2ec722af863af6e3a5e
Elise Yohm <elisery@hotmail.com>
2017-10-18 21:17:58 -0700

    Complete add and remove functions

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,17 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { searchResults:
+      [ {name: 'Rise Up', artist: 'Andra Day', album: 'Cheers to the Fall'},
+        {name: 'Lovesick', artist: 'Jacob Whitesides', album: 'Lovesick - Single'},
+        {name: 'ILYSB', artist: 'LANY', album: 'Make Out'}
+      ],
+      playlistName: 'My list',
+      playlistTracks:
+      [ {name: 'On a Good Day', artist: 'Oceanlab', album: 'Anjunabeats'},
+        {name: 'Oceans Away', artist: 'ARIZONA', album: 'Oceans Away'},
+        {name: 'The Best Crew', artist: 'Tep No', album: 'The Best Crew - Single'}
+      ]
+    };
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+  }
+
+  addTrack(track) {
+    var checkTrack = this.playlistTracks.find(theTrack => theTrack.id === track.id);
+    if (checkTrack === null) {
+      let newList = this.state.playlistTracks.concat(track);
+      this.setState({playlistTracks: newList});
+    }
+  }
+
+  removeTrack(track) {
+    //var checkTrack = this.playlistTracks.find(theTrack => theTrack.id === track.id);
+    //if (checkTrack !== null) {
+    let newList = this.playlistTracks.filter(theTrack => theTrack.id !== track.id);
+    this.setState({playlistTracks: newList});
+    //}
+  }
+
   render() {
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          <!-- Add a SearchBar component -->
+          <SearchBar />
           <div className="App-playlist">
-          <!-- Add a SearchResults component -->
-          <!-- Add a Playlist component -->
+          <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
+          <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}
+            onRemove={this.removeTrack}/>
           </div>
         </div>
       </div>
